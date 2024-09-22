c1a1b590cf1c21e1497c40faabd8370773a4edb8
kencottrell <cottrell.ken@gmail.com>
2024-09-22 14:30:49 -0500

    Update aira-events.py
    update to use camera event table

--- aira-events.py
+++ aira-events.py
@@ -12,14 +12,15 @@ url = "https://x8ki-letl-twmt.n7.xano.io/api:8GpeBcyt/event"
 
 
 data = {
-        "created_at": ts,
-        "hotels_id": 59,
-        "person_id": 59,
-        "geo_point": 0.0
+        "camera_timestamp": ts,
+        "hotel_id": 59,
+        "camera_id": 59,
+        "location_id": 12
     }
 
 data_json = json.dumps(data)
 headers = {'Content-type': 'application/json'}
 
 response = requests.post(url, data=data_json, headers=headers)
+print(data_json)
 print(response)
