f902cf39bca038dd6d52127c149e137f167cd9eb
kencottrell <cottrell.ken@gmail.com>
2024-09-24 17:35:06 -0500

    added get buildings call to Situm
    need to figure out how to prettify response.text into JSON

--- aira-situm-xano-events.py
+++ aira-situm-xano-events.py
@@ -11,19 +11,25 @@ timenow = ct.strftime("%H:%M:%S")
 
 # URL to Xano test API endpoing. Public
 
-url = "https://x8ki-letl-twmt.n7.xano.io/api:8GpeBcyt/event"
+xanourl = "https://x8ki-letl-twmt.n7.xano.io/api:8GpeBcyt/event"
+situm_url = 'https://dashboard.situm.com/api/v1/buildings'
 
-
-data = {
+xanodata = {
         "camera_timestamp": timenow,
         "hotel_id": 100,
         "camera_id": 9,
         "location_id": 125
     }
 
-data_json = json.dumps(data)
+data_json = json.dumps(xanodata)
 headers = {'Content-type': 'application/json'}
 
-response = requests.post(url, data=data_json, headers=headers)
+response = requests.post(xanourl, data=data_json, headers=headers)
 print(data_json)
-print(response)
+print('Xano: ' + str(response))
+
+headers = {'Content-type': 'application/json', 
+                     'X-API-KEY' :'e0c828ad924a10e7450d894d3a304db7d6dafa916f26c49c6cba698be80fc7b3' }
+response = requests.get(situm_url, headers=headers)
+payload = json.dumps(response.text, indent=6)
+print(response.text)
