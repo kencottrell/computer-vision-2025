08cfb82e77065bfdfdc6b187b18d386ffde1b035
kencottrell <cottrell.ken@gmail.com>
2024-11-01 12:25:16 -0500

    added opencv test file
    opencv test file

---
+++ opencv1.py
@@ -0,0 +1,49 @@
+# from the geeks for geeks 
+# open cv tutorial https://www.geeksforgeeks.org/extract-images-from-video-in-python/
+
+# py -m pip install opencv-python   -- this is the only command that works!! have to use "py" 
+#   not pythong 
+
+import cv2
+import os
+print(os.path)
+print(cv2.__version__)
+
+# Read the video from specified path 
+# cam = cv2.VideoCapture("C:\\Users\\Admin\\PycharmProjects\\project_1\\openCV.mp4") 
+cam = cv2.VideoCapture("C:\\Users\\kjcot\\mp4files\\aira-sample.mp4")
+try: 
+      
+    # creating a folder named data 
+    if not os.path.exists("C:\\Users\\kjcot\\mp4files\\data"): 
+        os.makedirs("C:\\Users\\kjcot\\mp4files\\data") 
+  
+# if not created then raise error 
+except OSError: 
+    print ('Error: Creating directory of data') 
+  
+# frame 
+currentframe = 0
+i = 0  
+while(i < 5): 
+    i=i+1  
+    # reading from frame 
+    ret,frame = cam.read() 
+  
+    if ret: 
+        # if video is still left continue creating images 
+        name = "C:\\Users\\kjcot\\mp4files\\data\\" + str(currentframe) + '.jpg'
+        print ('Creating...' + name) 
+  
+        # writing the extracted images 
+        cv2.imwrite(name, frame) 
+  
+        # increasing counter so that it will 
+        # show how many frames are created 
+        currentframe += 1
+    else: 
+        break
+  
+# Release all space and windows once done 
+cam.release() 
+cv2.destroyAllWindows() 
