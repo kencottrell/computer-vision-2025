e0ac4d8f3dfa7bdeff04eaff54503bc653bc8993
kencottrell <cottrell.ken@gmail.com>
2024-11-11 13:37:28 -0600

    more directory tweaks
    file redo for aira xano stium apis

--- inputs/video-input-settings.py
+++ inputs/video-input-settings.py
@@ -7,8 +7,11 @@ debug = False
 
 inputdir =  "C:\\Users\\kjcot\\mp4files\\"
 outputdir = inputdir + "data\\"
+
 inputname = 'guest-checkin'
 
 # set url to 0 for webcam, otherwise mp4 file name
 ip_camera_url = inputdir + inputname + '.mp4'
+
 ip_camera_url = 0
+inputname = 'kens-webcam'
--- ipcam/ipcam-output-rstp-display-by-frame.py
+++ ipcam/ipcam-output-rstp-display-by-frame.py
@@ -2,6 +2,7 @@ import cv2
 
 # Replace with your RTSP stream URL
 rtsp_url = 'rtsp://username:password@ip_address:port/endpoint'
+rtsp_url = 0
 
 # Open the RTSP stream
 cap = cv2.VideoCapture(rtsp_url)
@@ -9,7 +10,7 @@ cap = cv2.VideoCapture(rtsp_url)
 if not cap.isOpened():
     print("Error: Cannot open RTSP stream")
     exit()
-
+i=0
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -17,7 +18,11 @@ while True:
         break
 
     # Display the resulting frame
-    cv2.imshow('RTSP Stream', frame)
+    if i == 0: 
+        print('frame size, show just once: ' + str(frame.size))
+        i = i + 1
+    if(i < 20):
+        cv2.imshow('RTSP Stream', frame)
 
     # Press 'q' to exit the loop
     if cv2.waitKey(1) & 0xFF == ord('q'):
