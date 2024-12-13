86bcf7180a63f2837a8450098981161e8a3804e2
kencottrell <cottrell.ken@gmail.com>
2024-12-13 16:02:33 -0600

    file uploader added

--- aws-rekognition/sample2.py
+++ aws-rekognition/sample2.py
@@ -10,9 +10,12 @@ sys.path.append('inputs')
 inputs_module = importlib.import_module('video-inout-settings')
 classes_module = importlib.import_module('classes')
 
-debug = True
+debug = False
 aws_access_key_id = ''     # UNUSED , makes use of AWS CLI, otherwise place value here
 aws_secret_access_key = ''   # makes use of AWS CLI creds, otherwise place value here
+
+upload_test_file = 'C:\\Users\\kjcot\\mp4files\\testfile.txt'
+
 
 image_file = 'hotel-people.jpg'
 #image_file = 'manyfaces.jpg'
@@ -27,7 +30,6 @@ arn = 'arn:aws:s3:::' + input_bucket + '/' + image_file
 object_url = 'https://' + input_bucket+ '.s3.us-east-2.amazonaws.com/' + image_file
 
 
-
 # rek = boto3.client('rekognition', 
 #                           aws_access_key_id=aws_access_key_id,
 #                           aws_secret_access_key=aws_secret_access_key,
@@ -37,11 +39,14 @@ object_url = 'https://' + input_bucket+ '.s3.us-east-2.amazonaws.com/' + image_f
 s3 = boto3.client('s3', region_name=region_name)
 rek = boto3.client('rekognition', region_name=region_name)
 kvs = boto3.client("kinesisvideo", region_name=region_name )
-print('rekognition Endpoint: ' + str(rek._endpoint))
-print('kinesis endpoint: ' + str(kvs._endpoint))
-print('s3 endpoint: ' + str(s3._endpoint))
 
-response = s3.list_objects(Bucket=input_bucket)
+if debug:
+    print('rekognition Endpoint: ' + str(rek._endpoint))
+    print('kinesis endpoint: ' + str(kvs._endpoint))
+    print('s3 endpoint: ' + str(s3._endpoint))
+    response = s3.list_objects(Bucket=input_bucket)
+
+s3.upload_file(upload_test_file, input_bucket, 'hello.txt')
 
 def detect_faces(image_path):
     with open(image_path, 'rb') as image_file:
