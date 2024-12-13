290fbf057027d2ed218ef2eb7193d41006135e81
kencottrell <cottrell.ken@gmail.com>
2024-12-13 13:15:50 -0600

    merge AWS rekognition class files

---
+++ aws-rekognition/classes/RekognitionCollection.py
@@ -0,0 +1,208 @@
+class RekognitionCollection:
+    """
+    Encapsulates an Amazon Rekognition collection. This class is a thin wrapper
+    around parts of the Boto3 Amazon Rekognition API.
+    """
+
+    def __init__(self, collection, rekognition_client):
+        """
+        Initializes a collection object.
+
+        :param collection: Collection data in the format returned by a call to
+                           create_collection.
+        :param rekognition_client: A Boto3 Rekognition client.
+        """
+        self.collection_id = collection["CollectionId"]
+        self.collection_arn, self.face_count, self.created = self._unpack_collection(
+            collection
+        )
+        self.rekognition_client = rekognition_client
+
+    @staticmethod
+    def _unpack_collection(collection):
+        """
+        Unpacks optional parts of a collection that can be returned by
+        describe_collection.
+
+        :param collection: The collection data.
+        :return: A tuple of the data in the collection.
+        """
+        return (
+            collection.get("CollectionArn"),
+            collection.get("FaceCount", 0),
+            collection.get("CreationTimestamp"),
+        )
+
+
+    def describe_collection(self):
+        """
+        Gets data about the collection from the Amazon Rekognition service.
+
+        :return: The collection rendered as a dict.
+        """
+        try:
+            response = self.rekognition_client.describe_collection(
+                CollectionId=self.collection_id
+            )
+            # Work around capitalization of Arn vs. ARN
+            response["CollectionArn"] = response.get("CollectionARN")
+            (
+                self.collection_arn,
+                self.face_count,
+                self.created,
+            ) = self._unpack_collection(response)
+            logger.info("Got data for collection %s.", self.collection_id)
+        except ClientError:
+            logger.exception("Couldn't get data for collection %s.", self.collection_id)
+            raise
+        else:
+            return self.to_dict()
+
+
+    def search_faces(self, face_id, threshold, max_faces):
+        """
+        Searches for faces in the collection that match another face from the
+        collection.
+
+        :param face_id: The ID of the face in the collection to search for.
+        :param threshold: The match confidence must be greater than this value
+                          for a face to be included in the results.
+        :param max_faces: The maximum number of faces to return.
+        :return: The list of matching faces found in the collection. This list does
+                 not contain the face specified by `face_id`.
+        """
+        try:
+            response = self.rekognition_client.search_faces(
+                CollectionId=self.collection_id,
+                FaceId=face_id,
+                FaceMatchThreshold=threshold,
+                MaxFaces=max_faces,
+            )
+            faces = [RekognitionFace(face["Face"]) for face in response["FaceMatches"]]
+            logger.info(
+                "Found %s faces in %s that match %s.",
+                len(faces),
+                self.collection_id,
+                face_id,
+            )
+        except ClientError:
+            logger.exception(
+                "Couldn't search for faces in %s that match %s.",
+                self.collection_id,
+                face_id,
+            )
+            raise
+        else:
+            return faces
+
+
+    def list_faces(self, max_results):
+        """
+        Lists the faces currently indexed in the collection.
+
+        :param max_results: The maximum number of faces to return.
+        :return: The list of faces in the collection.
+        """
+        try:
+            response = self.rekognition_client.list_faces(
+                CollectionId=self.collection_id, MaxResults=max_results
+            )
+            faces = [RekognitionFace(face) for face in response["Faces"]]
+            logger.info(
+                "Found %s faces in collection %s.", len(faces), self.collection_id
+            )
+        except ClientError:
+            logger.exception(
+                "Couldn't list faces in collection %s.", self.collection_id
+            )
+            raise
+        else:
+            return faces
+
+
+   def index_faces(self, image, max_faces):
+        """
+        Finds faces in the specified image, indexes them, and stores them in the
+        collection.
+
+        :param image: The image to index.
+        :param max_faces: The maximum number of faces to index.
+        :return: A tuple. The first element is a list of indexed faces.
+                 The second element is a list of faces that couldn't be indexed.
+        """
+        try:
+            response = self.rekognition_client.index_faces(
+                CollectionId=self.collection_id,
+                Image=image.image,
+                ExternalImageId=image.image_name,
+                MaxFaces=max_faces,
+                DetectionAttributes=["ALL"],
+            )
+            indexed_faces = [
+                RekognitionFace({**face["Face"], **face["FaceDetail"]})
+                for face in response["FaceRecords"]
+            ]
+            unindexed_faces = [
+                RekognitionFace(face["FaceDetail"])
+                for face in response["UnindexedFaces"]
+            ]
+            logger.info(
+                "Indexed %s faces in %s. Could not index %s faces.",
+                len(indexed_faces),
+                image.image_name,
+                len(unindexed_faces),
+            )
+        except ClientError:
+            logger.exception("Couldn't index faces in image %s.", image.image_name)
+            raise
+        else:
+            return indexed_faces, unindexed_faces
+
+
+
+    def search_faces_by_image(self, image, threshold, max_faces):
+        """
+        Searches for faces in the collection that match the largest face in the
+        reference image.
+
+        :param image: The image that contains the reference face to search for.
+        :param threshold: The match confidence must be greater than this value
+                          for a face to be included in the results.
+        :param max_faces: The maximum number of faces to return.
+        :return: A tuple. The first element is the face found in the reference image.
+                 The second element is the list of matching faces found in the
+                 collection.
+        """
+        try:
+            response = self.rekognition_client.search_faces_by_image(
+                CollectionId=self.collection_id,
+                Image=image.image,
+                FaceMatchThreshold=threshold,
+                MaxFaces=max_faces,
+            )
+            image_face = RekognitionFace(
+                {
+                    "BoundingBox": response["SearchedFaceBoundingBox"],
+                    "Confidence": response["SearchedFaceConfidence"],
+                }
+            )
+            collection_faces = [
+                RekognitionFace(face["Face"]) for face in response["FaceMatches"]
+            ]
+            logger.info(
+                "Found %s faces in the collection that match the largest "
+                "face in %s.",
+                len(collection_faces),
+                image.image_name,
+            )
+        except ClientError:
+            logger.exception(
+                "Couldn't search for faces in %s that match %s.",
+                self.collection_id,
+                image.image_name,
+            )
+            raise
+        else:
+            return image_face, collection_faces
+
+
---
+++ aws-rekognition/classes/RekognitionCollectionManager.py
@@ -0,0 +1,57 @@
+class RekognitionCollectionManager:
+    """
+    Encapsulates Amazon Rekognition collection management functions.
+    This class is a thin wrapper around parts of the Boto3 Amazon Rekognition API.
+    """
+
+    def __init__(self, rekognition_client):
+        """
+        Initializes the collection manager object.
+
+        :param rekognition_client: A Boto3 Rekognition client.
+        """
+        self.rekognition_client = rekognition_client
+
+
+    def create_collection(self, collection_id):
+        """
+        Creates an empty collection.
+
+        :param collection_id: Text that identifies the collection.
+        :return: The newly created collection.
+        """
+        try:
+            response = self.rekognition_client.create_collection(
+                CollectionId=collection_id
+            )
+            response["CollectionId"] = collection_id
+            collection = RekognitionCollection(response, self.rekognition_client)
+            logger.info("Created collection %s.", collection_id)
+        except ClientError:
+            logger.exception("Couldn't create collection %s.", collection_id)
+            raise
+        else:
+            return collection
+
+    def list_collections(self, max_results):
+        """
+        Lists collections for the current account.
+
+        :param max_results: The maximum number of collections to return.
+        :return: The list of collections for the current account.
+        """
+        try:
+            response = self.rekognition_client.list_collections(MaxResults=max_results)
+            collections = [
+                RekognitionCollection({"CollectionId": col_id}, self.rekognition_client)
+                for col_id in response["CollectionIds"]
+            ]
+        except ClientError:
+            logger.exception("Couldn't list collections.")
+            raise
+        else:
+            return collections
+
+
+
+
