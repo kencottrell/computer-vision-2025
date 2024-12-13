27b07d8cbf7be0d7941dea2ff2f361f7a6648b03
kencottrell <cottrell.ken@gmail.com>
2024-12-13 15:31:21 -0600

    uploaded image, video to s3, more code to check

--- aws-rekognition/sample2.py
+++ aws-rekognition/sample2.py
@@ -3,30 +3,45 @@ import sys
 import importlib
 import cv2
 import os
-print(os.path)
-print(cv2.__version__)
+print('OS Path: ' + str(os.path))
+print('OpenCV version: ' + cv2.__version__)
 
 sys.path.append('inputs')
 inputs_module = importlib.import_module('video-inout-settings')
 classes_module = importlib.import_module('classes')
 
 debug = True
-aws_access_key_id = ''
-aws_secret_access_key = ''
-if debug:
-    rek = boto3.client('rekognition', region_name="us-east-1")
-else:
-    rek2 = boto3.client('rekognition', 
-                           aws_access_key_id=aws_access_key_id,
-                           aws_secret_access_key=aws_secret_access_key,
-                           region_name='us-east-1')
+aws_access_key_id = ''     # UNUSED , makes use of AWS CLI, otherwise place value here
+aws_secret_access_key = ''   # makes use of AWS CLI creds, otherwise place value here
 
-kvs = boto3.client("kinesisvideo", region_name="us-east-1" )
+image_file = 'hotel-people.jpg'
+#image_file = 'manyfaces.jpg'
+video_file = '3peoplewalking-vert.mp4'
+#video_file = '3peoplewalking-horiz.mp4'
+
+region_name = 'us-east-2'
+input_bucket = 'kens-input-files-bucket'
+
+s3uri = 's3://' + input_bucket + '/' + image_file
+arn = 'arn:aws:s3:::' + input_bucket + '/' + image_file
+object_url = 'https://' + input_bucket+ '.s3.us-east-2.amazonaws.com/' + image_file
 
 
+
+# rek = boto3.client('rekognition', 
+#                           aws_access_key_id=aws_access_key_id,
+#                           aws_secret_access_key=aws_secret_access_key,
+#                            region_name='us-east-1')
+
+
+s3 = boto3.client('s3', region_name=region_name)
+rek = boto3.client('rekognition', region_name=region_name)
+kvs = boto3.client("kinesisvideo", region_name=region_name )
 print('rekognition Endpoint: ' + str(rek._endpoint))
 print('kinesis endpoint: ' + str(kvs._endpoint))
+print('s3 endpoint: ' + str(s3._endpoint))
 
+response = s3.list_objects(Bucket=input_bucket)
 
 def detect_faces(image_path):
     with open(image_path, 'rb') as image_file:
