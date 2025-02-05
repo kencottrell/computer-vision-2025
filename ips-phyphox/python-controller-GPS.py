f73f8bd05544988a0037e2c7f1bfc6cfb222dd0e
kencottrell <cottrell.ken@gmail.com>
2025-02-05 13:27:39 -0600

    reading lon, lat, z, t from gps buffer

--- ips-phyphox/python-controller-GPS.py
+++ ips-phyphox/python-controller-GPS.py
@@ -60,6 +60,64 @@ def filter_data(data):
     Output:  [0.02, 0.03]
     '''
 
+def get_config(config):
+    try:
+        print("get Config settings: " + config) 
+        response = requests.get(f"{PHY_ADDRESS}{config}")
+        if response.status_code == 200:
+            print(response.json())  # {"result": true} if successful
+        else:
+            print(f"Error: {response.status_code}")
+    except Exception as e:
+        print(f"Failed to send command: {e}")
+
+def export():
+    while True:
+        response = requests.get(f"{PHY_ADDRESS}/export?format=0")
+        with open("data.csv", "a") as file:
+            file.write(response.text)
+        requests.get(f"{PHY_ADDRESS}/control?cmd=clear")
+        time.sleep(5)  # Adjust interval as needed
+
+
+def poll_gps_data():
+    #CHANNELS = ["latitude", "longitude", "altitude"]  # Example channels for GPS data
+    CHANNELS = ["lat", "lon", "z", "t"]  # Example channels for GPS data
+    count = 0
+    while True:
+        try:
+        # Build the URL for data retrieval
+            url = f"{PHY_ADDRESS}/get?" + "&".join(CHANNELS)
+            response = requests.get(url)
+            data = response.json()
+        
+
+        # Process the received data
+            print("-----")
+            for channel in CHANNELS:
+                values = data["buffer"][channel]["buffer"]
+                print(f"{channel}: {values[-1]}")  # Print the most recent value
+               
+
+    
+        except Exception as e:
+            print("Error:", e)
+        count = count + 1
+       
+
+        time.sleep(1)  # Adjust interval as needed
+        '''
+            for filtering:
+            To use the /get command in Phyphox for retrieving GPS data via the remote interface, you can specify the desired buffers in your request. For example:
+            Basic Request: /get?latitude&longitude
+            This retrieves the latest latitude and longitude values.
+            Full Buffer: /get?latitude=full&longitude=full
+            This returns the full buffer of recorded latitude and longitude values.
+            Threshold-Based: /get?latitude=42&longitude=42|time
+            This fetches latitude and longitude values starting from the timestamp where the "time" buffer exceeds 42.
+            Make sure to encode special characters like | as %7C in the URL if needed
+            '''
+
 
 
 def send_command(command):
@@ -129,4 +187,7 @@ if single_pass:    # start experiment, allow remote access and wait for code to 
     send_command("stop")   # Stop the experiment
     send_command("clear")
 else :    # start experiment and leave running
+    config = "/config"
+    # get_config(config)
+    poll_gps_data()
     fetch_data()
