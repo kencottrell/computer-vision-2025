47236d5fef242e795274c79409370a5f59fc56d6
kencottrell <cottrell.ken@gmail.com>
2025-07-20 13:56:13 -0500

    Create xano-button-press-events.py

---
+++ xano/xano-button-press-events.py
@@ -0,0 +1,30 @@
+# py -m pip install requests  -- this is the only command that works!!
+import requests
+import json
+#import time
+import datetime
+ct = datetime.datetime.now()
+
+situmkey = 'e0c828ad924a10e7450d894d3a304db7d6dafa916f26c49c6cba698be80fc7b3'
+ts = ct.timestamp()
+timenow = ct.strftime("%H:%M:%S")
+
+# URL to Xano test API endpoing. Public
+
+
+url ="https://x8ki-letl-twmt.n7.xano.io/api:dptOGfrO/smartwatchalertbuttonevents"
+
+
+data = {
+        "deviceID": timenow,
+        "latitude": 59.59,
+        "longitude": 89.54,
+        "altitude": 12.0
+    }
+
+data_json = json.dumps(data)
+headers = {'Content-type': 'application/json'}
+
+response = requests.post(url, data=data_json, headers=headers)
+print(data_json)
+print(response)
